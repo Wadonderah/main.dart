7e63cab0221ebb7a7cf4bb3bded3e6474c6b571a
Wadonderah <wadondera@gmail.com>
2024-02-29 22:46:50 +0300

    commit

---
+++ Lists.dart
@@ -0,0 +1,9 @@
+void main() {
+  List<String> names = ["John", "James", "Peter"];
+  print("Value of names is $names");
+  print("Value of names[0] is ${names[0]}");
+  print("value of names[1] is %{names[1]}");
+  print("Value of names[2] is $names[2]");
+
+  print(names);
+}
