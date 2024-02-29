4d8f20aa1ec5a84293b749497b3bc279c8a46405
Wadonderah <wadondera@gmail.com>
2024-02-29 23:50:15 +0300

    commit

---
+++ Arithmetic.dart
@@ -0,0 +1,22 @@
+void main() {
+  int num1=20;
+  int num2=7;
+
+  int sum=num1+num2;
+  int diff=num1-num2;
+  int subtract = num2-num1;
+  int mul=num1*num2;
+  double div=num1/num2;
+  int div2 =num1~/num2;
+  int mod=num1%num2;
+
+  print("The addition is $sum.");
+  print("The subtraction is $diff.");
+  print("The unary minus is $subtract.");
+  print("The multiplication is $mul.");
+  print("The division is $div.");
+  print("The integer division is $div2.");
+  print("The modulus is $mod.");
+
+
+}
