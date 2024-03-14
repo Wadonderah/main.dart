e03f700682861d10b36dd67dae53ba0c1e516c02
Wadonderah <wadondera@gmail.com>
2024-03-14 11:01:01 +0300

    commit

---
+++ Class_area.dart
@@ -0,0 +1,23 @@
+import 'dart:math';
+
+class Area {
+  // properties
+  double length;
+  double width;
+  
+  // constructor
+  Area(this.length, this.width);
+  
+  // method to calculate area
+  double calculateArea() {
+    return length * width;
+  }
+}
+
+void main() {
+  // Creating an instance of the Area class
+  var rectangle = Area(5, 3);
+
+  // Calculating and printing the area
+  print('Area of the rectangle: ${rectangle.calculateArea()} square units');
+}
